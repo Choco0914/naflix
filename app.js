fdc6d04146b0df3b8323505b41d811149a47c293
Choco0914 <heojeongho1992@gmail.com>
2018-12-21 14:58:13 +0900

    Express Core:Routing

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { userRouter } from "./router";
 
 const app = express();
-
-const PORT = 4000;
 
 const handleListening = () =>
   console.log(`✅ Listening on: http://localhost:${PORT}`);
@@ -27,4 +26,6 @@ app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+app.use("/user", userRouter);
+
+export default app;
