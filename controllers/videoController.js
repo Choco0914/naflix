d6783b0182fbf20d35f86ed8bfe6765f0166c120
Choco0914 <heojeongho1992@gmail.com>
2018-12-22 18:07:09 +0900

    Search Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,10 +1,19 @@
 export const home = (req, res) => res.render("home", { pageTitle: "Home" });
-export const search = (req, res) =>
-  res.render("search", { pageTitle: "search" });
+
+export const search = (req, res) => {
+  const {
+    query: { video: searchingBy }
+  } = req;
+  res.render("search", { pageTitle: "Search", searchingBy });
+};
+
 export const videos = (req, res) =>
   res.render("videos", { pageTitle: "Videos" });
+
 export const originals = (req, res) =>
   res.render("originals", { pageTitle: "Originals" });
+
 export const trailers = (req, res) =>
   res.render("trailers", { pageTitle: "Trailers" });
+
 export const show = (req, res) => res.render("show", { pageTitle: "Show" });
